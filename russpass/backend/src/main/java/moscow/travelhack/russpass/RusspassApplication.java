7ab34a0c28d93096900796b63351a4b3f40a6f38
ArtemShashulovskiy <stratosphere665@gmail.com>
2021-03-27 12:12:45 +0300

    gitignore update

---
+++ russpass/backend/src/main/java/moscow/travelhack/russpass/RusspassApplication.java
@@ -0,0 +1,13 @@
+package moscow.travelhack.russpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RusspassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RusspassApplication.class, args);
+	}
+
+}
---
+++ russpass/backend/src/test/java/moscow/travelhack/russpass/RusspassApplicationTests.java
@@ -0,0 +1,13 @@
+package moscow.travelhack.russpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RusspassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
