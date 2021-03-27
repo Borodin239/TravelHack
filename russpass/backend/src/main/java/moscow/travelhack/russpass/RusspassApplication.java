270dc463fbba8f54ff53b1e3d81e74993eaad244
Lana243 <lana0771711@gmail.com>
2021-03-27 21:49:51 +0500

    OAuth

--- russpass/backend/src/main/java/moscow/travelhack/russpass/RusspassApplication.java
+++ russpass/backend/src/main/java/moscow/travelhack/russpass/RusspassApplication.java
@@ -2,24 +2,16 @@ package moscow.travelhack.russpass;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
-@EnableAuthorizationServer
-@EnableResourceServer
 
-public class RusspassApplication {
-
+public class RusspassApplication extends WebSecurityConfigurerAdapter {
 	public static void main(String[] args) {
 		SpringApplication.run(RusspassApplication.class, args);
-	}
-
-	@RequestMapping(value = "/products")
-	public String getProductName() {
-		return "Honey";
 	}
 }
