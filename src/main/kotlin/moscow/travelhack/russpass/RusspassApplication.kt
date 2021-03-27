18b45c7b51b39da75d5d713dd09d65ef42f875f5
ArtemShashulovskiy <stratosphere665@gmail.com>
2021-03-27 11:43:32 +0300

    Project setup

---
+++ src/main/kotlin/moscow/travelhack/russpass/RusspassApplication.kt
@@ -0,0 +1,11 @@
+package moscow.travelhack.russpass
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RusspassApplication
+
+fun main(args: Array<String>) {
+	runApplication<RusspassApplication>(*args)
+}
